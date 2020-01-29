541fe54c045a535584f7339f8ce35ec1743aae5a
Ella Zhang <ellazhang37@gmail.com>
2020-01-28 22:17:10 -0500

    + Added idle and walk animation

--- TinyHero/Assets/Movement.cs
+++ TinyHero/Assets/Movement.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    Rigidbody2D rb;
+
+    [SerializeField] float walkSpeed;
+
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        FlipSprite();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Walk();
+        if(Input.GetAxis("Horizontal") != 0f)
+        {
+            animator.SetBool("Moving", true);
+        }
+        else
+        {
+            animator.SetBool("Moving", false);
+        }
+    }
+
+    void Walk()
+    {
+        float walkInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(walkInput * walkSpeed, rb.velocity.y);
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1f);
+        }
     }
 }
