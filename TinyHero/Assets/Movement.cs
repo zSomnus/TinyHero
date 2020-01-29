e87dfd8432ae7a1188575a65792b9772da6a1949
Ella <ellazhang37@gmail.com>
2020-01-29 10:44:35 -0500

    + Added Jump() and simulate physics

--- TinyHero/Assets/Movement.cs
+++ TinyHero/Assets/Movement.cs
@@ -7,6 +7,14 @@ public class Movement : MonoBehaviour
     Rigidbody2D rb;
 
     [SerializeField] float walkSpeed;
+    [Space]
+    [SerializeField] float jumpSpeed;
+    [SerializeField] float minJumpSpeed;
+    [SerializeField] float maxFallSpeed;
+    [SerializeField] float fallMultiplier;
+    [SerializeField] float lowJumpMultiplier;
+
+
 
     Animator animator;
     // Start is called before the first frame update
@@ -34,12 +42,40 @@ public class Movement : MonoBehaviour
         {
             animator.SetBool("Moving", false);
         }
+        Jump();
+
+        SimulatePhysics();
+    }
+
+    void SimulatePhysics()
+    {
+        // simulate physics
+        if (rb.velocity.y < minJumpSpeed && rb.velocity.y > -maxFallSpeed)
+        {
+
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
     }
 
     void Walk()
     {
         float walkInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(walkInput * walkSpeed, rb.velocity.y);
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            //Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+            Debug.Log("Jump()");
+        }
     }
 
     void FlipSprite()
