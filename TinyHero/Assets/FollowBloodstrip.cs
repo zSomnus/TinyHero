9ead32569e7d58217f7c6282b365cc6142708d29
Ella Zhang <ellazhang37@gmail.com>
2020-02-10 00:23:18 -0500

    Attack logic

---
+++ TinyHero/Assets/FollowBloodstrip.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowBloodstrip : MonoBehaviour
+{
+
+    [SerializeField] Transform target;
+    Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            Vector2 pos = mainCamera.WorldToScreenPoint(target.position);
+            transform.position = pos;
+        }
+    }
+}
