3648f73bb02dc6d046c7a8b0d7fd6484e3c37557
Ella Zhang <ellazhang37@gmail.com>
2020-02-28 23:07:25 -0500

    Slime movement

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    protected bool playerInRange;
+    protected bool onGround;
 
+    protected Vector2 spawnPoint;
+    [SerializeField] protected LayerMask playerLayer;
+
+    [SerializeField] protected Vector2 movingRange;
+    [SerializeField] protected Vector2 rangeOffset;
     protected Rigidbody2D rb;
     [SerializeField] protected int damage;
     protected int hp;
@@ -14,6 +21,8 @@ public class Enemy : MonoBehaviour
     protected Animator animator;
     [SerializeField] protected float deathDelay;
 
+    protected GameObject hero;
+
 
     // Start is called before the first frame update
     protected virtual void Start()
@@ -21,14 +30,11 @@ public class Enemy : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         hp = maxHp;
         animator = GetComponent<Animator>();
+        spawnPoint = rb.transform.position;
+        hero = GameObject.Find("Hero");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     internal virtual void TakeDamage(int damage)
     {
         hp -= damage;
@@ -43,5 +49,19 @@ public class Enemy : MonoBehaviour
         animator.SetTrigger("Die");
         Debug.Log("(X_X)");
         Destroy(this.gameObject, delay);
+    }
+    protected void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon + 0.5f;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1f);
+        }
+    }
+
+    protected void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(new Vector2(rangeOffset.x + transform.position.x, rangeOffset.y + transform.position.y), movingRange);
     }
 }
--- Assets/Script/Slime.cs
+++ Assets/Script/Slime.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Slime : Enemy
 {
+    BoxCollider2D collider;
+
     // Start is called before the first frame update
     protected override void Start()
     {
         animator = GetComponent<Animator>();
         maxHp = 1;
+        collider = GetComponent<BoxCollider2D>();
         base.Start();
     }
 
@@ -16,5 +19,34 @@ public class Slime : Enemy
     void Update()
     {
         animator.SetInteger("HP", base.hp);
+        FlipSprite();
+        playerInRange = Physics2D.OverlapBox((Vector2)transform.position + rangeOffset, movingRange, 0f, playerLayer);
+        CatchPlayer();
+    }
+
+    void CatchPlayer()
+    {
+        if (playerInRange)
+        {
+            Debug.Log("Moving");
+            if (hero.transform.position.x - transform.position.x > collider.size.x / 2f)    //transform.position.x + this.collider.size.x / 2f
+            {
+                Debug.Log("Slime Move Right");
+                animator.SetBool("Move", true);
+                rb.velocity = new Vector2(speed, rb.velocity.y);
+            }
+            else if (hero.transform.position.x - transform.position.x < -collider.size.x / 2f)
+            {
+                Debug.Log("Slime Move Left");
+                animator.SetBool("Move", true);
+                rb.velocity = new Vector2(-speed, rb.velocity.y);
+            }
+            else
+            {
+                Debug.Log("Slime Stoped");
+                animator.SetBool("Move", false);
+                rb.velocity = Vector2.zero;
+            }
+        }
     }
 }
