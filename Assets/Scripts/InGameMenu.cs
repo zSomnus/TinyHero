a2fd30c72554321ada0fe1c979758ae8e14f7c10
Nicolas <derekhasun@gmail.com>
2020-03-01 02:05:18 -0500

    InGameMenu + Fixes
    add InGameMenu
    fix main menu script

---
+++ Assets/Scripts/InGameMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameMenu : MonoBehaviour
+{
+    public static bool PausedGame = false;
+    public GameObject InGameMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (PausedGame)
+            {
+                Resume();
+            }
+            else 
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume() 
+    {
+        InGameMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        PausedGame = false;
+    }
+
+    void Pause() 
+    {
+        InGameMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        PausedGame = true;
+    }
+
+    public void Retry() 
+    {
+        SceneManager.LoadScene("SampleScene");
+        Time.timeScale = 1f;
+    }
+
+    public void QuitGame() 
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
