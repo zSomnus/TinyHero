7fcf21741a237493a21752a3ce723a75b23ccf66
Nicolas <derekhasun@gmail.com>
2020-03-02 08:10:36 -0500

    Trap Script
    add spike sprite
    add trap script

---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    public int damage = 1;
+    protected GameObject hero;
+
+    void Start()
+    {
+        hero = GameObject.Find("Hero");
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            hero.GetComponent<Hero>().TakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
