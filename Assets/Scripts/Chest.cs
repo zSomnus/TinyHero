b83d5a36926b27a5c9573a1182823fa0951141a1
Nicolas <derekhasun@gmail.com>
2020-03-07 15:58:25 -0500

    Chest Script + New Potion and Chest
    Added Chest Script
    Added Big potion
    Added Chest Prefab

---
+++ Assets/Scripts/Chest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    public GameObject itemSpawn;
+    private bool Spawned = false;
+    public Sprite sprite1;
+    public Sprite sprite2;
+
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer.sprite == null)
+            spriteRenderer.sprite = sprite1;
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            ChangeSprite();
+            if (Spawned == false)
+                Instantiate(itemSpawn);
+            Spawned = true;
+        }
+    }
+
+    void ChangeSprite()
+    {
+        if (spriteRenderer.sprite == sprite1)
+        {
+            spriteRenderer.sprite = sprite2;
+        }
+        else
+        {
+            spriteRenderer.sprite = sprite1;
+        }
+    }
+}
