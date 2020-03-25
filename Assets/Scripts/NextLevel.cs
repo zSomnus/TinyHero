f551e4e05ad9613047ce5c8257bd00f1d14547fc
Nicolas <derekhasun@gmail.com>
2020-03-25 00:19:13 -0400

    Main Menu + Level 1, 2, 3 Updates + Next Level + Exits

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
@@ -14,5 +15,13 @@ public class NextLevel : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        }
     }
 }
